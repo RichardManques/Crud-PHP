36b8fd30836686a39d12be200821093abf0b1b26
Richard Manques <manques11@gmail.com>
2020-10-31 16:30:36 -0300

    Funciones CRUD y tabla

--- index.php
+++ index.php
@@ -1,32 +1,78 @@
 <?php
-    ini_set('display_errors',1);
+    //INSTRUCCIONES PARA MOSTRAR POSIBLES ERRORES PHP
+use models\TareaModel as TareaModel;
+
+ini_set('display_errors',1);
     ini_set('display_startup_errors',1);
     error_reporting(E_ALL);
-?>
+    require_once("models/TareaModel.php");//IMPORTANDO LA TAREA MODEL
+    
+    $model = new TareaModel();
+    $tareas = $model->TareaList();
+    print_r($tareas);
+    $prueba = $model->searchTask(2);
+    print_r($prueba);
 
+    $model->deleteTask(2);
+    $model->updateTask(3,['nombre'=>'Tarea #3','descripcion'=>'Estudiar Materialize']);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
 <body>
-    <form action="controllers/ControlInsert.php" method="POST">
-        <input type="text" name="nombre" placeholder="Nombre de la tarea">
-        <br>
-        <input type="text" name="desc" placeholder="Descripcion de la tarea">
-        <br>
-        <button>Guardar tarea</button>
-    </form>
-    <p>
-        <?php
-            session_start();
-            if(isset($_SESSION['resp'])){
-                echo $_SESSION['resp'];
-                unset($_SESSION['resp']);
-            }
-        ?>
-    </p>
+    <div class="container">
+        <div class="row">
+            <div class="col l4 m4 s12">
+                <h4 class="center">Nueva Tarea</h4>
+                <form action="controllers/ControlInsert.php" method="POST">
+                    <div class="input-field">
+                        <input id="nombre" type="text" name="nombre">
+                        <label for="nombre">Nombre</label>
+                    </div>
+                    <div class="input-field">
+                        <input id="descripcion" type="text" name="desc">
+                        <label for="descripcion">Descripcion</label>
+                    </div>
+                    <button class="btn">Guardar tarea</button>
+                </form>
+                <p>
+                    <?php
+                        session_start();
+                        if(isset($_SESSION['resp'])){
+                            echo $_SESSION['resp'];
+                            unset($_SESSION['resp']);
+                        }
+                    ?>
+                </p>
+            </div>
+            <div class="col l8 m8 s12">
+                <h4 class="center">Listado de Tareas</h4>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Tarea</th>
+                        <th>Descripcion</th>
+                    </tr>
+                    <?php foreach ($tareas as $item) { ?>
+                        <tr>
+                            <td><?=$item["id"]?></td>
+                            <td><?=$item["nombre"]?></td>
+                            <td><?=$item["descripcion"]?></td>
+                            <td>
+                                <button class="btn">Editar</button>
+                                <button class="btn">Eliminar</button>
+                            </td>
+                        </tr>    
+                    <?php } ?>
+                </table>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
--- models/TareaModel.php
+++ models/TareaModel.php
@@ -6,10 +6,33 @@ require_once("Conexion.php");
 class TareaModel{
 
     public function insertar($data){ //$data es un arreglo asociativo, generado de otra parte (controlador)
-        $stm = Conexion::conector()->prepare("INSERT INTO TAREAS VALUES(NULL,:nombre,:descripcion)");
+        $stm = Conexion::conector()->prepare("INSERT INTO tareas VALUES(NULL,:nombre,:descripcion)");
         $stm->bindParam(":nombre",$data['nombre']);
         $stm->bindParam(":descripcion",$data['descripcion']);
         return $stm->execute();
     }
 
+    public function TareaList(){
+        $stm = Conexion::conector()->prepare("SELECT * FROM tareas");
+        $stm->execute();
+        return $stm->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    public function searchTask($id){
+        $stm = Conexion::conector()->prepare("SELECT * FROM tareas WHERE id=:A");
+        $stm->bindParam(":A",$id);
+        $stm->execute();
+        return $stm->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    public function deleteTask($id){
+        $stm = Conexion::conector()->prepare("DELETE FROM tareas WHERE id=:A");
+        $stm->bindParam(":A",$id);
+        return $stm->execute();
+    }
+    public function updateTask($id, $data){ //$data["nombre"=>valor1,"descrip"=>jaskjd]
+        $stm = Conexion::conector()->prepare("UPDATE tareas SET nombre=:A, descripcion:=:B WHERE id=:C");
+        $stm->bindParam(":A",$data['nombre']);
+        $stm->bindParam(":B",$data['descripcion']);
+        $stm->bindParam(":C",$id);
+        return $stm->execute();
+    }
 }
